d822e3b3be44ee3dbccc0b3746eb30a7dc81ca1a
stephaneworkspace <s.bressani@bluewin.ch>
2020-11-25 09:09:11 +0100

    simple iteration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+const VEC_NOTE: [&str; 12] = [
+    "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B",
+];
+
+const VEC_CHORD: [&str; 13] = [
+    "", "m", "sus2", "sus4", "aug", "dim", "7", "m7", "maj7", "aug7",
+    "augMaj7", "dim7", "m7b5",
+]; // TODO in chartgeneratorsvg a list
+
 fn main() {
-    println!("Hello, world!");
+    let mut vec_note_chord: Vec<String> = Vec::new();
+    for note in VEC_NOTE.iter() {
+        for chord in VEC_CHORD.iter() {
+            vec_note_chord.push(format!("{}{}", note, chord));
+        }
+    }
+    for action in vec_note_chord.iter() {
+        println!("{}", action);
+    }
 }
